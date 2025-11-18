fe6ffe8dba587f5c17407d01cc311c5b7789b042
David <davidkuron1996@gmail.com>
2025-11-18 21:30:05 +0300

    add this file

--- lib/car.py
+++ lib/car.py
@@ -1,4 +1,7 @@
 from vehicle import Vehicle
-
 class Car(Vehicle):
+    def go(self):
+        return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+    
+    # fill_up_tank method is inherited from Vehicle and doesn't need to be redefined
     pass
